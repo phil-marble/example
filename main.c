c8cfc886818c84f0658c679b0c89ebd60312a687
Philip Freeman <phil@marble.io>
2018-08-03 11:52:28 -0700

    add a bunch of alternate messages...

--- main.c
+++ main.c
@@ -4,13 +4,23 @@
 int
 main ()
 {
-  if (rand () % 2 == 0)
+  switch (rand () % 4)
     {
+    case 0:
       printf ("hello world\n");
-    }
-  else
-    {
+      break;
+    case 1:
       printf ("howdy world\n");
+      break;
+    case 2:
+      printf ("hola mundo\n");
+      break;
+    case 3:
+      printf ("yo bro\n");
+      break;
+    case 4:
+      printf ("yowza!\n");
+      break;
     }
   return 0;
 }
